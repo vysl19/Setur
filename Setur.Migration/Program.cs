1d1751291e3e95447c58ed0d5cf3d28ce4bfd433
veysel kurhan <vysl19@gmail.com>
2021-03-14 01:50:06 +0300

    migration tool is developed

--- Setur.Migration/Program.cs
+++ Setur.Migration/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Configuration;
+using System.IO;
+using System.Reflection;
+using Npgsql;
 
 namespace Setur.Migration
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {    
+            var connectionString= ConfigurationManager.ConnectionStrings["PostgreConnection"].ConnectionString;
+            string path = Path.Combine(Environment.CurrentDirectory.Replace("\\bin\\Debug\\netcoreapp3.1",""), @"sql.txt");            
+            var sql= File.ReadAllText(path);
+            if (!string.IsNullOrEmpty(sql))
+            {
+                using (NpgsqlConnection conn = new NpgsqlConnection(connectionString))
+                using (NpgsqlCommand cmd = new NpgsqlCommand())
+                {
+                    conn.Open();
+                    cmd.Connection = conn;
+                    cmd.CommandText = sql;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            
         }
     }
 }
